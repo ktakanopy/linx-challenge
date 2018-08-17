59c2624120580feac8242e72c2a204ad211aade2
Kevin Takano <kevin.takano@gmail.com>
2018-08-17 17:51:53 -0400

    [FOURTH DAY]
    - Transformed the DataFrame to show the region using join and select.

--- src/main/scala/ignition/jobs/SparkTestJob.scala
+++ src/main/scala/ignition/jobs/SparkTestJob.scala
@@ -11,11 +11,32 @@ object SparkTestJob {
         dfTransformed
   }
 
-  def transform(df: DataFrame): DataFrame = {
+  def addRegionState(df: DataFrame, stateCityRegionDf: DataFrame) : DataFrame = {
+
+      var resultDf = df.join(stateCityRegionDf, df("addresses").getField("city")(0) === stateCityRegionDf("city") && df("addresses").getField("state")(0) === stateCityRegionDf("state") ).drop("city").drop("state")
+      .select(df("cpf"), df("name") , df("email"), df("gender"), df("birthDate"), df("age") , df("maritalStatus"), df("phones")
+      // )
+        , df("addresses.addressType"), df("addresses.city"), df("addresses.complement"), df("addresses.country")
+        , stateCityRegionDf("region")
+        , df("addresses.district"), df("addresses.city"), df("addresses.complement"), df("addresses.country")
+        , df("addresses.number"), df("addresses.state"), df("addresses.street"), df("addresses.zipcode") 
+      )
+
+      resultDf.printSchema()
+      resultDf.show(false)
+
+      // val res = resultDf.select("city")
+
+      resultDf
+  }
+
+  def transform(df: DataFrame,  stateCityRegionDf: DataFrame ): DataFrame = {
         println("SparkTestJob.transform()")
         // add age
 
         val dfTransformed = addAgeColumn(df)
+
+        val dfRes = addRegionState(dfTransformed, stateCityRegionDf)
 
         // df.select("addresses.city").show()
 
--- src/main/scala/ignition/jobs/setups/SparkTestSetup.scala
+++ src/main/scala/ignition/jobs/setups/SparkTestSetup.scala
@@ -1,5 +1,6 @@
 package ignition.jobs.setups
 
+import org.apache.commons.lang3._
 
 import play.api.libs.json._
 
@@ -10,6 +11,7 @@ import ignition.core.jobs.utils.SparkContextUtils._
 import ignition.core.jobs.utils.SimplePathDateExtractor.default
 
 import org.apache.spark.sql.DataFrame 
+import org.apache.spark.sql.SQLContext
 
 object SparkTestSetup {
 
@@ -19,23 +21,24 @@ object SparkTestSetup {
     println("SparkTestSetup.calcHistogram()")
   }
 
-  def setupStateCityData() {
+  def setupStateCityRegionData(sqlContext : SQLContext) : DataFrame = {
 
     val cityPath = workDirectory + "/sample/state-city/state-city.json"
 
-    val cityContent = scala.io.Source.fromFile(cityPath).mkString
+    val cityDf = sqlContext.read.json(cityPath)
 
-    val citiesList = Json.parse(cityContent).as[List[Map[String,String]]]
+    cityDf.show()
 
-    val stateCitiesMap = scala.collection.mutable.Map[String, List[String]]()
+    cityDf.printSchema()
 
+    cityDf
   }
 
   def run(runnerContext: RunnerContext) {
     println("SparkTestSetup.run()")
     val sc = runnerContext.sparkContext
     val sparkConfig = runnerContext.config
-    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+    val sqlContext = new SQLContext(sc)
 
     // val lines: RDD[String] = sc.textFile(Seq("file:///home/ktakano/work/neemu/linx-challenge/sample/*.json"))
 
@@ -45,11 +48,11 @@ object SparkTestSetup {
 
     val df = sqlContext.read.json(path)
 
-    setupStateCityData()
+    df.printSchema()
 
-    val dfTransform : DataFrame = SparkTestJob.transform(df)
+    val stateCityRegionDf = setupStateCityRegionData(sqlContext)
 
-    calcHistogram(dfTransform)
+    val dfTransform : DataFrame = SparkTestJob.transform(df, stateCityRegionDf)
 
     // val top1000Words: Seq[(Int, String)] = wordCount
       // .map { case (word, count) => (count, word) }
